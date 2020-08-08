2ad28a158b2a020c41c3e96b1e88a289e02da691
박상민 <shadowpur97@gmail.com>
2020-08-08 13:36:09 +0000

    Commit Test

--- cache/csim.c
+++ cache/csim.c
@@ -4,8 +4,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+typedef struct{
+	int valid;
+}cache_line;
+
+typedef struct{
+	int valid;
+}cache_set;
+
 int main()
 {
     printSummary(0, 0, 0);
-    return 0;
+	return 1;
 }
